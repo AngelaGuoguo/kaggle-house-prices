70f406cb8a717374cf42b1acd861b7b1051560b5
dydokamil <dydokamil@gmail.com>
2017-02-19 12:26:27 +0100

    Added batch generation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
@@ -18,9 +18,31 @@ def oh_encode(data_frame):
     return data_encoded
 
 
+def batch_generator(data_frame_encoded):
+    """Generates data to be fed to the neural network."""
+    labels = data_frame_encoded['SalePrice'][0]
+    del data_frame_encoded['SalePrice']
+    data = np.array(list(data_frame_encoded.values()))[:, 0]
+    data = [np.array(d) for d in data]
+
+    NUM_FEATURES = len(data)
+    NUM_BATCHES = len(data[0])
+    for i in range(NUM_BATCHES):
+        batch_compiled = []
+        for j in range(NUM_FEATURES):
+            if type(data[j][i]) is np.ndarray:
+                batch_compiled.extend(data[j][i])
+            else:
+                batch_compiled.extend([data[j][i]])
+        yield batch_compiled, labels[i]
+
+
 df_train = pd.read_csv('./data/train.csv', keep_default_na=False)
 # drop useless data
 df_train = df_train.drop(['Street', 'LotFrontage', 'LandSlope', 'YearBuilt', 'YearRemodAdd',
                           'MasVnrArea', 'Foundation', 'GarageYrBlt', 'MoSold', 'YrSold'], 1)
 df_train_encoded = oh_encode(df_train)
-# print(df_train_encoded)
+batch_gen = batch_generator(df_train_encoded)
+# print(next(batch_gen))
+# print('*'*8)
+# print(next(batch_gen))
