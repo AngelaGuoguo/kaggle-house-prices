a0019a0e817faecede45e0a9a1c7e0cb9855a895
dydokamil <dydokamil@gmail.com>
2017-02-21 21:45:28 +0100

    Save best models

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
 
-TRAINING_EXAMPLES = 700
-NUM_EPOCHS = 500000
-HIDDEN_SIZE = 100
-num_features = 52
+TRAINING_EXAMPLES = 128
+NUM_EPOCHS = 16000
+HIDDEN_SIZE = 500
+num_features = 835
 
 
 def oh_encode(data_frame):
@@ -52,17 +52,12 @@ def batch_generator(data_frame_encoded):
 
 df_train = pd.read_csv('./data/train.csv', keep_default_na=False)
 # drop useless data
-# df_train = df_train.drop(['Id', 'Street', 'LotFrontage', 'LandSlope', 'YearBuilt', 'YearRemodAdd',
-#                           'MasVnrArea', 'Foundation', 'GarageYrBlt', 'MoSold', 'YrSold',
-#                           'MSZoning', 'Alley', 'LotShape', 'LandContour', 'Utilities',
-#                           'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2',
-#                           ], 1)
-df_train = df_train[['MSSubClass', 'LotArea', 'OverallQual', 'OverallCond', 'ExterQual',
-                     'ExterCond', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea',
-                     'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr',
-                     'KitchenQual', 'Functional', 'GarageType', 'WoodDeckSF', 'OpenPorchSF',
-                     'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'Fence', 'SalePrice']]
-# df_train = df_train.drop(['Id'], 1)
+# df_train = df_train[['LotArea', 'OverallQual', 'OverallCond', 'ExterQual', 'ExterCond', '1stFlrSF', '2ndFlrSF',
+#                      'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
+#                      'BedroomAbvGr', 'KitchenAbvGr', 'KitchenQual', 'Functional', 'GarageType', 'WoodDeckSF',
+#                      'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'Fence',
+#                      'GarageArea', 'SalePrice']]
+df_train = df_train.drop(['Id'], 1)
 column_names = df_train.columns.values
 df_train_encoded = oh_encode(df_train)
 df_train_encoded_normalized = normalize(df_train_encoded)
@@ -71,25 +66,24 @@ batch_gen = batch_generator(df_train_encoded_normalized)
 
 # create the neural network model
 keep_prob = tf.placeholder(tf.float32)
-gamma = tf.constant(.83, tf.float32)
-learning_rate = tf.Variable(1e-1, trainable=False)
+# learning_rate = tf.Variable(1e-1, trainable=False)
 prev_loss = tf.Variable(0., trainable=False)
 
 input_layer = tf.placeholder(tf.float32, [None, num_features])
-W1 = tf.Variable(tf.random_normal([num_features, HIDDEN_SIZE], stddev=.1))
-b1 = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=.1))
+W1 = tf.Variable(tf.random_normal([num_features, HIDDEN_SIZE], stddev=.01))
+b1 = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=.01))
 h1_layer = tf.matmul(input_layer, W1) + b1
 h1_layer = tf.nn.relu(h1_layer)
 h1_layer = tf.nn.dropout(h1_layer, keep_prob)
 
-W2 = tf.Variable(tf.random_normal([HIDDEN_SIZE, HIDDEN_SIZE], stddev=.1))
-b2 = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=.1))
+W2 = tf.Variable(tf.random_normal([HIDDEN_SIZE, HIDDEN_SIZE], stddev=.01))
+b2 = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=.01))
 h2_layer = tf.matmul(h1_layer, W2) + b2
 h2_layer = tf.nn.relu(h2_layer)
 h2_layer = tf.nn.dropout(h2_layer, keep_prob)
 
-W3 = tf.Variable(tf.random_normal([HIDDEN_SIZE, 1], stddev=.1))
-b3 = tf.Variable(tf.random_normal([1], stddev=.1))
+W3 = tf.Variable(tf.random_normal([HIDDEN_SIZE, 1], stddev=.01))
+b3 = tf.Variable(tf.random_normal([1], stddev=.01))
 output_layer = tf.reduce_sum(tf.matmul(h2_layer, W3) + b3)
 y = tf.placeholder(tf.float32, shape=[None, 1])
 
@@ -98,7 +92,10 @@ loss = tf.subtract(y, output_layer)
 loss = tf.square(loss)
 loss = tf.reduce_sum(loss)
 loss = tf.divide(loss, 2.)
-optimizer = tf.train.AdadeltaOptimizer(learning_rate=learning_rate).minimize(loss)
+reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+reg_constant = 0.01  # Choose an appropriate one.
+loss = loss + reg_constant * sum(reg_losses)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-7).minimize(loss)
 
 sess = tf.Session()
 
@@ -116,6 +113,8 @@ valid_batches = np.array(valid_examples[:, 0])
 valid_len = len(valid_batches)
 valid_batches = np.concatenate(valid_batches)
 valid_batches = np.reshape(valid_batches, [valid_len, -1])
+saver = tf.train.Saver(max_to_keep=15)
+min_loss = 50000000
 
 # get the next batch
 for i in range(NUM_EPOCHS):
@@ -128,21 +127,14 @@ for i in range(NUM_EPOCHS):
     train_batches = np.concatenate(train_batches)
     train_batches = np.reshape(train_batches, [TRAINING_EXAMPLES, -1])
     # feed the batch
-    _, train_loss, lr = sess.run([optimizer, loss, learning_rate], feed_dict={input_layer: train_batches,
-                                                                              y: train_labels, keep_prob: .2})
-
+    _, train_loss = sess.run([optimizer, loss], feed_dict={input_layer: train_batches, y: train_labels, keep_prob: .5})
+    valid_loss = sess.run(loss, feed_dict={input_layer: valid_batches, y: valid_labels, keep_prob: 1.})
     # log results
-    if i % 1000 == 0:  # validate on the validation dataset
-        valid_loss = sess.run(loss, feed_dict={input_layer: valid_batches, y: valid_labels, keep_prob: 1.})
-        print('epoch: {}, train loss: {}, valid loss: {}, learning rate: {}'.format(i,
-                                                                                    train_loss,
-                                                                                    valid_loss,
-                                                                                    lr))
-    if i % 200 == 0:
-        previous_loss = sess.run(prev_loss)
-        if valid_loss > previous_loss:
-            sess.run(learning_rate.assign(gamma * learning_rate))
-            previous_loss = sess.run(prev_loss)
-        sess.run(prev_loss.assign(valid_loss))
+    if i % 1000 == 0:
+        print('epoch: {}, train loss: {}, valid loss: {}'.format(i, train_loss, valid_loss))
+    if valid_loss < min_loss:
+        min_loss = valid_loss
+        save_path = saver.save(sess, "./saves/model{}.ckpt".format(valid_loss))
+        print('Best model so far: loss: {}, epoch: {}'.format(valid_loss, i))
 
 sess.close()
