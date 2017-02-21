de4bc0ed98b46a6a5b8d3a1f45fcfdd98b046a61
dydokamil <dydokamil@gmail.com>
2017-02-21 19:20:02 +0100

    Added gamma, LR decay

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
 
-TRAINING_EXAMPLES = 100
+TRAINING_EXAMPLES = 700
 NUM_EPOCHS = 500000
-LEARNING_RATE = 1e-2
 HIDDEN_SIZE = 100
-num_features = 298
-DROPOUT_PROB = .2
+num_features = 52
 
 
 def oh_encode(data_frame):
@@ -17,7 +15,7 @@ def oh_encode(data_frame):
     for feature in data_frame:
         data_i = df_train[feature]
         encoder = None
-        if df_train[feature].dtype == 'O' or feature == "MSSubClass":  # is data categorical?
+        if df_train[feature].dtype == 'O':  # is data categorical?
             encoder = LabelBinarizer()
             encoder.fit(list(set(df_train[feature])))
             data_i = encoder.transform(data_i)
@@ -54,8 +52,16 @@ def batch_generator(data_frame_encoded):
 
 df_train = pd.read_csv('./data/train.csv', keep_default_na=False)
 # drop useless data
-df_train = df_train.drop(['Id', 'Street', 'LotFrontage', 'LandSlope', 'YearBuilt', 'YearRemodAdd',
-                          'MasVnrArea', 'Foundation', 'GarageYrBlt', 'MoSold', 'YrSold'], 1)
+# df_train = df_train.drop(['Id', 'Street', 'LotFrontage', 'LandSlope', 'YearBuilt', 'YearRemodAdd',
+#                           'MasVnrArea', 'Foundation', 'GarageYrBlt', 'MoSold', 'YrSold',
+#                           'MSZoning', 'Alley', 'LotShape', 'LandContour', 'Utilities',
+#                           'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2',
+#                           ], 1)
+df_train = df_train[['MSSubClass', 'LotArea', 'OverallQual', 'OverallCond', 'ExterQual',
+                     'ExterCond', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea',
+                     'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr',
+                     'KitchenQual', 'Functional', 'GarageType', 'WoodDeckSF', 'OpenPorchSF',
+                     'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'Fence', 'SalePrice']]
 # df_train = df_train.drop(['Id'], 1)
 column_names = df_train.columns.values
 df_train_encoded = oh_encode(df_train)
@@ -64,18 +70,23 @@ df_train_encoded_normalized = normalize(df_train_encoded)
 batch_gen = batch_generator(df_train_encoded_normalized)
 
 # create the neural network model
+keep_prob = tf.placeholder(tf.float32)
+gamma = tf.constant(.83, tf.float32)
+learning_rate = tf.Variable(1e-1, trainable=False)
+prev_loss = tf.Variable(0., trainable=False)
+
 input_layer = tf.placeholder(tf.float32, [None, num_features])
 W1 = tf.Variable(tf.random_normal([num_features, HIDDEN_SIZE], stddev=.1))
 b1 = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=.1))
 h1_layer = tf.matmul(input_layer, W1) + b1
 h1_layer = tf.nn.relu(h1_layer)
-h1_layer = tf.nn.dropout(h1_layer, DROPOUT_PROB)
+h1_layer = tf.nn.dropout(h1_layer, keep_prob)
 
 W2 = tf.Variable(tf.random_normal([HIDDEN_SIZE, HIDDEN_SIZE], stddev=.1))
 b2 = tf.Variable(tf.random_normal([HIDDEN_SIZE], stddev=.1))
 h2_layer = tf.matmul(h1_layer, W2) + b2
 h2_layer = tf.nn.relu(h2_layer)
-h2_layer = tf.nn.dropout(h2_layer, DROPOUT_PROB)
+h2_layer = tf.nn.dropout(h2_layer, keep_prob)
 
 W3 = tf.Variable(tf.random_normal([HIDDEN_SIZE, 1], stddev=.1))
 b3 = tf.Variable(tf.random_normal([1], stddev=.1))
@@ -87,7 +98,7 @@ loss = tf.subtract(y, output_layer)
 loss = tf.square(loss)
 loss = tf.reduce_sum(loss)
 loss = tf.divide(loss, 2.)
-optimizer = tf.train.AdadeltaOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
+optimizer = tf.train.AdadeltaOptimizer(learning_rate=learning_rate).minimize(loss)
 
 sess = tf.Session()
 
@@ -117,10 +128,21 @@ for i in range(NUM_EPOCHS):
     train_batches = np.concatenate(train_batches)
     train_batches = np.reshape(train_batches, [TRAINING_EXAMPLES, -1])
     # feed the batch
-    _, train_loss = sess.run([optimizer, loss], feed_dict={input_layer: train_batches, y: train_labels})
+    _, train_loss, lr = sess.run([optimizer, loss, learning_rate], feed_dict={input_layer: train_batches,
+                                                                              y: train_labels, keep_prob: .2})
+
     # log results
     if i % 1000 == 0:  # validate on the validation dataset
-        valid_loss = sess.run(loss, feed_dict={input_layer: valid_batches, y: valid_labels})
-        print('epoch: {}, train loss: {}, valid loss: {}'.format(i, train_loss, valid_loss))
+        valid_loss = sess.run(loss, feed_dict={input_layer: valid_batches, y: valid_labels, keep_prob: 1.})
+        print('epoch: {}, train loss: {}, valid loss: {}, learning rate: {}'.format(i,
+                                                                                    train_loss,
+                                                                                    valid_loss,
+                                                                                    lr))
+    if i % 200 == 0:
+        previous_loss = sess.run(prev_loss)
+        if valid_loss > previous_loss:
+            sess.run(learning_rate.assign(gamma * learning_rate))
+            previous_loss = sess.run(prev_loss)
+        sess.run(prev_loss.assign(valid_loss))
 
 sess.close()
